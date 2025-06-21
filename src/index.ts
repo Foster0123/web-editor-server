5f69419055a561aee39b3da942360b810d565043
Foster Z <72973015+Foster0123@users.noreply.github.com>
2025-06-21 15:25:11 +0530

    feat: move the compilation logic to editor.controller.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,19 @@
 import { Elysia, t } from "elysia";
 import { cors } from '@elysiajs/cors';
 
-const app = new Elysia()
+import { languageCompiler } from "./controllers/editor.controller";
 
-app.post("/api", async ({ body }) => {
-	const filePath = `${process.cwd()}\\temp\\main.js`;
-	const writePath = `${process.cwd()}\\temp\\output.txt`;
-	if (body.lang === 'javascript') {
+const app = new Elysia({ serve: { hostname: process.env.HOST }})
 
-		Bun.write(filePath, body.code)
-
-		const proc = Bun.spawn(['node', filePath], { stderr: 'pipe' })
-		const output = await new Response(proc.stdout).text()
-		const errors = await Bun.readableStreamToText(proc.stderr)
-
-		if (errors) {
-			return { output: errors }
-		}
-		return { output: output }
-	}
-	if (body.lang === 'python') {
-
-		Bun.write(filePath, body.code)
-
-		const proc = Bun.spawn(['python', filePath], { stderr: 'pipe' })
-		const output = await new Response(proc.stdout).text()
-		const errors = await Bun.readableStreamToText(proc.stderr)
-
-		if (errors) {
-			return { output: errors }
-		}
-		return { output: output }
-	}
-	if (body.lang === 'lua') {
-
-		Bun.write(filePath, body.code)
-
-		const proc = Bun.spawn(['lua', filePath], { stderr: 'pipe' })
-		const output = await new Response(proc.stdout).text()
-		const errors = await Bun.readableStreamToText(proc.stderr)
-
-		if (errors) {
-			return { output: errors }
-		}
-		return { output: output }
-	}
-}, 
+app.post("/api", async ({ body }) => languageCompiler({ body })), 
 	{
 		body: t.Object({
 			code: t.String(),
 			lang: t.String()
 		})
-	})
+	}
 
 app.use(cors())
-app.listen(3000);
+app.listen(process.env.PORT ?? 3000);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log(`Web Editor's Server is running at ${app.server?.hostname}:${app.server?.port}`);
